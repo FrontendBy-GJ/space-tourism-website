c6292c409e5c243957c034d3ebb572fe85a2b3fa
Genaro Garcia <genaro.dev@outlook.com>
2023-03-27 10:30:36 -0500

    change Link to NavLink, add style to current page

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import { hamburgerIcon, closeIcon, logo } from '../assets/shared';
 
 const Nav = () => {
@@ -15,6 +15,12 @@ const Nav = () => {
     }
   }
 
+  function getClassName({ isActive }) {
+    return isActive
+      ? `md:border-b-2 md:py-8`
+      : `md:py-8 md:inline-block md:hover:border-b-2 ${!nav && 'hidden'}`;
+  }
+
   return (
     <header className="fixed top-6 left-0 right-0 z-50 mx-auto flex  items-center justify-between px-7 md:pl-10 lg:top-10 3xl:pl-80">
       <Link to="/">
@@ -25,16 +31,13 @@ const Nav = () => {
         <ul
           className={`${
             nav ? 'translate-x-0' : 'translate-x-full md:translate-x-0'
-          } fixed inset-y-0 right-0 z-50 flex w-64 flex-col space-y-6 bg-white/5 px-8 pt-[18vh] font-barlow-condensed  uppercase text-light backdrop-blur-lg transition duration-300 ease-out  after:absolute after:-left-1/2 after:bottom-1/2 after:hidden after:h-0.5 after:w-full after:max-w-md after:-translate-x-2 after:translate-y-1/2  after:bg-white/25 md:h-24 md:w-[450px]  md:flex-row md:space-y-0 md:space-x-10 md:px-0 md:pt-0 xl:top-4 xl:w-[830px] after:xl:block after:3xl:max-w-4xl after:3xl:-translate-x-96`}
+          } fixed inset-y-0 right-0 z-50 flex w-64 flex-col space-y-6 bg-white/5 px-8 pt-[18vh] font-barlow-condensed uppercase text-light  backdrop-blur-lg transition duration-300 ease-out after:absolute after:-left-1/2  after:bottom-1/2 after:hidden after:h-0.5 after:w-full after:max-w-md after:-translate-x-2 after:translate-y-1/2 after:bg-white/25 md:h-24  md:w-[450px] md:flex-row md:items-center  md:space-y-0 md:space-x-10 md:px-0 md:pt-0 xl:top-4 xl:w-[830px] after:xl:block after:3xl:max-w-4xl after:3xl:-translate-x-96`}
         >
           {links.map((link) => (
-            <li
-              key={link.link}
-              className="md:first:ml-12 md:hover:border-b-2 xl:first:ml-32"
-            >
-              <Link
+            <li key={link.link} className="md:first:ml-12 xl:first:ml-32">
+              <NavLink
                 to={link.to}
-                className={`md:inline-block md:pt-9 ${!nav && 'hidden'}`}
+                className={getClassName}
                 onClick={toggleNav}
               >
                 <span
@@ -45,7 +48,7 @@ const Nav = () => {
                 </span>
 
                 {link.link}
-              </Link>
+              </NavLink>
             </li>
           ))}
         </ul>
